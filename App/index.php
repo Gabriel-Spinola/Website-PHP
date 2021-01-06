7087bd25cfa7d051ea43e71d5ef17379c269e99b
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-01-06 16:58:15 -0300

    Update index.php

--- App/index.php
+++ App/index.php
@@ -4,10 +4,10 @@
     <head>
 
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="keywords" content="keywords">
 
-        <title>Title</title>
+        <title>Commercial Site With PHP</title>
 
-        <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -18,9 +18,18 @@
 
     <body>
         
+        <header>
         
+            <div class="logo"></div>
 
-        <!-- Optional JavaScript -->
+            <nav></nav>
+
+        </header>
+
+        <main></main>
+
+        <footer></footer>
+
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
