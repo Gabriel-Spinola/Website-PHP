4d8277c8cbb61b4eef1145139191b7c981045db4
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-01-06 17:46:29 -0300

    Import Full Proj obs: wrong branch hehe

--- App/index.php
+++ App/index.php
@@ -1,214 +1,99 @@
-<!doctype html>
-<html lang="en">
-
+<?php 
+	include('config.php');
+?>
+<!DOCTYPE html>
+<html>
 <head>
-
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="keywords" content="keywords">
-
-    <title>Commercial Site With PHP</title>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+	<title>Projeto 01</title>
+	<link rel="stylesheet" href="estilo/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+	<link href="<?php echo INCLUDE_PATH; ?>estilo/style.css" rel="stylesheet" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="keywords" content="palavras-chave,do,meu,site">
+	<meta name="description" content="Descrição do meu website">
+	<link rel="icon" href="<?php echo INCLUDE_PATH; ?>favicon.ico" type="image/x-icon" />
+	<meta charset="utf-8" />
 </head>
-
 <body>
-        
-    <header>
-    
-        <div class="logo"></div>
+	<base base="<?php echo INCLUDE_PATH; ?>" />
+	<?php 
+		$url = isset($_GET['url']) ? $_GET['url'] : 'home';
 
-        <nav class="desktop">
-        
-            <ul>
-            
-                <li><a href="">Home</a></li>
-                <li><a href="">Sobre</a></li>
-                <li><a href="">Serviços</a></li>
-                <li><a href="">Contato</a></li>
-            
-            </ul>
-        
-        </nav><!--desktop-->
+		switch ($url) {
+			case 'depoimentos':
+				echo '<target target="depoimentos" />';
+				break;
 
-        <nav class="mobile">
-        
-            <ul>
-            
-                <li><a href="">Home</a></li>
-                <li><a href="">Sobre</a></li>
-                <li><a href="">Serviços</a></li>
-                <li><a href="">Contato</a></li>
-            
-            </ul>
-        
-        </nav><!--mobile-->
+			case 'servicos':
+				echo '<target target="servicos" />';
+				break;
+		}
+	?>
+	<header>
+		<div class="center">
+			<div class="logo left"><a href="<?php echo INCLUDE_PATH; ?>">Logomarca</a></div><!--logo-->
+			<nav class="desktop right">
+				<ul>
+					<li><a href="<?php echo INCLUDE_PATH; ?>">Home</a></li>
+					<li><a href="<?php echo INCLUDE_PATH; ?>depoimentos">Depoimentos</a></li>
+					<li><a href="<?php echo INCLUDE_PATH; ?>servicos">Serviços</a></li>
+					<li><a realtime="contato" href="<?php echo INCLUDE_PATH; ?>contato">Contato</a></li>
+				</ul>
+			</nav>
+			 <nav class="mobile right">
+			 	<div class="botao-menu-mobile">
+			 		<i class="fa fa-bars" aria-hidden="true"></i>
+			 	</div>
+				<ul>
+					<li><a href="<?php echo INCLUDE_PATH; ?>">Home</a></li>
+					<li><a href="<?php echo INCLUDE_PATH; ?>depoimentos">Depoimentos</a></li>
+					<li><a href="<?php echo INCLUDE_PATH; ?>servicos">Serviços</a></li>
+					<li><a realtime="contato" href="<?php echo INCLUDE_PATH; ?>contato">Contato</a></li>
+				</ul>
+			</nav>
+			<div class="clear"></div>
+		</div><!--center-->
+	</header>
 
-    </header>
+	<div class="container-principal">
+		<?php 
+			
+			if(file_exists('pages/'.$url.'.php')){
+				include('pages/'.$url.'.php');
+			}else{
+				if($url != 'depoimentos' && $url != 'servicos'){
+					$pagina404 = true;
+					include('pages/404.php');
+				}else{
+					include('pages/home.php');
+				}
+			}
+		?>
+	</div>
 
-    <main>
-    
-        <section class="banner-principal">
+	<footer <?php if(isset($pagina404) && $pagina404 == true) echo 'class="fixed"'; ?>>
+		<div class="center">
+			<p>Todos os direitos reservados</p>
+		</div><!--center-->
+	</footer>
 
-            <div class="center">
-        
-                <form>
-                
-                    <h2>Qual Seu Melhor E-mail</h2>
+	<script src="<?php echo INCLUDE_PATH; ?>js/jquery.js"></script>
+	<script src="<?php echo INCLUDE_PATH; ?>js/constants.js"></script>
+	<script src='https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDHPNQxozOzQSZ-djvWGOBUsHkBUoT_qH4'></script>
+	<script src="<?php echo INCLUDE_PATH; ?>js/map.js"></script>
+	<script src="<?php echo INCLUDE_PATH; ?>js/scripts.js"></script>
 
-                    <input type="email" name="email" required>
-                    <input type="submit" name="acao" value="Cadastrar!">
+	<?php
+		if($url == 'home' || $url == ''){
+	?>
+	<script src="<?php echo INCLUDE_PATH; ?>js/slider.js"></script>
+	<?php } ?>
+	
 
-                </form>
-
-            </div>
-
-        </section><!--banner-principal-->
-
-        <section class="descricao-autor">
-
-            <div class="center">
-
-                <div class="w50">
-
-                    <h2>Guilherme C. Grillo</h2>
-
-                    <article>
-                    
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere architecto in at sit assumenda repudiandae optio perferendis, libero blanditiis, cupiditate, nemo asperiores eveniet quae! Quaerat odio dolores laudantium quis consequuntur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis illum sunt ipsa numquam sed aliquid, veniam inventore molestiae magni hic ducimus, consectetur placeat nulla repellat labore error voluptatem voluptates. Sint. lor Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa corporis, perspiciatis exercitationem enim esse quisquam dolorem officia aliquam sapiente. Labore voluptate commodi nulla dolor assumenda autem repellat, voluptates repudiandae rerum! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex ipsa repudiandae esse modi qui alias beatae, et at, corporis nisi incidunt laudantium veritatis amet eius! Officia voluptatibus sapiente totam nesciunt.</p>
-
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel aperiam totam quasi veniam magnam, aliquam delectus odio adipisci dicta quos eum cumque dolor molestiae nam corrupti maxime ex quidem magni?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis provident ea error quos. Corrupti minus porro minima quis aspernatur a deleniti, unde nobis aliquid suscipit commodi id distinctio voluptatibus accusamus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora atque in impedit ab, praesentium dignissimos provident reiciendis possimus? Unde natus aperiam sunt debitis incidunt, iusto ut beatae impedit totam odio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste repudiandae illum adipisci reprehenderit perferendis explicabo facere amet aliquid quibusdam, excepturi obcaecati enim dolorem deserunt, id a aliquam magnam esse nulla.</p>
-                    
-                    </article>
-
-                </div><!--w50-->
-
-                <div class="w50">
-                
-                    <img src="images/foto.jpg">
-
-                </div><!--w50-->
-
-            </div><!--center-->
-
-        </section><!--banner-principal-->
-
-        <section class="especialidades">
-
-            <div class="center">
-
-                <h2 class="title">Especialidades</h2>
-            
-                <article class="box-especialidade">
-                
-                    <h3>icone1</h3>
-                    <h3>CSS3</h3>
-
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dolore ea delectus repudiandae iure ipsam magni, aperiam veritatis voluptate facere odit saepe hic necessitatibus suscipit ducimus eaque assumenda illum debitis.</p>
-                
-                </article><!--box-especialidade-->
-
-                <article class="box-especialidade">
-                
-                    <h3>icone2</h3>
-                    <h3>HTML</h3>
-
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dolore ea delectus repudiandae iure ipsam magni, aperiam veritatis voluptate facere odit saepe hic necessitatibus suscipit ducimus eaque assumenda illum debitis.</p>
-                
-                </article><!--box-especialidade-->
-
-                <article class="box-especialidade">
-                
-                    <h3>icone3</h3>
-                    <h3>JavaScript</h3>
-
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dolore ea delectus repudiandae iure ipsam magni, aperiam veritatis voluptate facere odit saepe hic necessitatibus suscipit ducimus eaque assumenda illum debitis.</p>
-                
-                </article><!--box-especialidade-->
-            
-            </div><!--center-->
-
-        </section><!--especialidades-->
-
-        <section class="extras">
-        
-            <div class="center">
-
-                <article class="w50">
-
-                    <h2 class="title">Depoimentos</h2>
-
-                    <article class="depoimento-single">
-                    
-                        <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus iste dicta vero illum, vel, minima cumque id reprehenderit blanditiis distinctio soluta ad, ipsa consectetur aut nobis numquam doloribus aspernatur quae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur necessitatibus harum voluptatibus repellendus temporibus saepe eum, corrupti eveniet commodi quia ex porro voluptates quos minima repellat, ipsum deleniti. Nulla?</p>
-
-                        <p class="nome-auto">Lorem Ipsum</p>
-                    
-                    </article><!--depoimento-single-->
-
-                    <article class="depoimento-single">
-                    
-                        <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus iste dicta vero illum, vel, minima cumque id reprehenderit blanditiis distinctio soluta ad, ipsa consectetur aut nobis numquam doloribus aspernatur quae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur necessitatibus harum voluptatibus repellendus temporibus saepe eum, corrupti eveniet commodi quia ex porro voluptates quos minima repellat, ipsum deleniti. Nulla?</p>
-
-                        <p class="nome-auto">Lorem Ipsum</p>
-                    
-                    </article><!--depoimento-single-->
-
-                    <article class="depoimento-single">
-                    
-                        <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus iste dicta vero illum, vel, minima cumque id reprehenderit blanditiis distinctio soluta ad, ipsa consectetur aut nobis numquam doloribus aspernatur quae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur necessitatibus harum voluptatibus repellendus temporibus saepe eum, corrupti eveniet commodi quia ex porro voluptates quos minima repellat, ipsum deleniti. Nulla?</p>
-
-                        <p class="nome-auto">Lorem Ipsum</p>
-                    
-                    </article><!--depoimento-single-->
-
-                </article><!--w50-->
-
-                <article class="w50">
-
-                    <h2 class="title">Serviços</h2>
-
-                    <div class="servicos">
-
-                        <ul>
-                        
-                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus iste dicta vero illum, vel, minima cumque id reprehenderit blanditiis distinctio soluta ad, ipsa consectetur aut nobis numquam doloribus aspernatur quae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur necessitatibus harum voluptatibus repellendus temporibus saepe eum, corrupti eveniet commodi quia ex porro voluptates quos minima repellat, ipsum deleniti. Nulla?</li>
-
-                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus iste dicta vero illum, vel, minima cumque id reprehenderit blanditiis distinctio soluta ad, ipsa consectetur aut nobis numquam doloribus aspernatur quae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur necessitatibus harum voluptatibus repellendus temporibus saepe eum, corrupti eveniet commodi quia ex porro voluptates quos minima repellat, ipsum deleniti. Nulla?</li>
-                            
-                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus iste dicta vero illum, vel, minima cumque id reprehenderit blanditiis distinctio soluta ad, ipsa consectetur aut nobis numquam doloribus aspernatur quae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur necessitatibus harum voluptatibus repellendus temporibus saepe eum, corrupti eveniet commodi quia ex porro voluptates quos minima repellat, ipsum deleniti. Nulla?</li>
-                        
-                        </ul>
-
-                    </div><!--servicos-->
-
-                </article><!--w50-->
-
-            </div><!--center-->
-        
-        </section><!--extras-->
-
-    </main>
-
-    <footer>
-
-        <div class="center">
-
-            <p>Todos os Direitos Reservados &copy;</p>
-
-        </div>
-    
-    </footer>
-
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
+	<?php
+		if($url == 'contato'){
+	?>
+	<?php } ?>
+	<script src="<?php echo INCLUDE_PATH; ?>js/exemplo.js"></script>
 </body>
-
 </html>
