29135bd36085e3ad3446a8d8d69341f0cb375bc2
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-01-06 17:37:48 -0300

    finalization of content

--- App/index.php
+++ App/index.php
@@ -1,40 +1,214 @@
 <!doctype html>
 <html lang="en">
 
-    <head>
+<head>
 
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="keywords" content="keywords">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="keywords">
 
-        <title>Commercial Site With PHP</title>
+    <title>Commercial Site With PHP</title>
 
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    </head>
+</head>
 
-    <body>
+<body>
         
-        <header>
+    <header>
+    
+        <div class="logo"></div>
+
+        <nav class="desktop">
         
-            <div class="logo"></div>
+            <ul>
+            
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+            
+            </ul>
+        
+        </nav><!--desktop-->
 
-            <nav></nav>
+        <nav class="mobile">
+        
+            <ul>
+            
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+            
+            </ul>
+        
+        </nav><!--mobile-->
 
-        </header>
+    </header>
 
-        <main></main>
+    <main>
+    
+        <section class="banner-principal">
 
-        <footer></footer>
+            <div class="center">
+        
+                <form>
+                
+                    <h2>Qual Seu Melhor E-mail</h2>
 
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+                    <input type="email" name="email" required>
+                    <input type="submit" name="acao" value="Cadastrar!">
 
-    </body>
+                </form>
+
+            </div>
+
+        </section><!--banner-principal-->
+
+        <section class="descricao-autor">
+
+            <div class="center">
+
+                <div class="w50">
+
+                    <h2>Guilherme C. Grillo</h2>
+
+                    <article>
+                    
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere architecto in at sit assumenda repudiandae optio perferendis, libero blanditiis, cupiditate, nemo asperiores eveniet quae! Quaerat odio dolores laudantium quis consequuntur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis illum sunt ipsa numquam sed aliquid, veniam inventore molestiae magni hic ducimus, consectetur placeat nulla repellat labore error voluptatem voluptates. Sint. lor Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa corporis, perspiciatis exercitationem enim esse quisquam dolorem officia aliquam sapiente. Labore voluptate commodi nulla dolor assumenda autem repellat, voluptates repudiandae rerum! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex ipsa repudiandae esse modi qui alias beatae, et at, corporis nisi incidunt laudantium veritatis amet eius! Officia voluptatibus sapiente totam nesciunt.</p>
+
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel aperiam totam quasi veniam magnam, aliquam delectus odio adipisci dicta quos eum cumque dolor molestiae nam corrupti maxime ex quidem magni?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis provident ea error quos. Corrupti minus porro minima quis aspernatur a deleniti, unde nobis aliquid suscipit commodi id distinctio voluptatibus accusamus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora atque in impedit ab, praesentium dignissimos provident reiciendis possimus? Unde natus aperiam sunt debitis incidunt, iusto ut beatae impedit totam odio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste repudiandae illum adipisci reprehenderit perferendis explicabo facere amet aliquid quibusdam, excepturi obcaecati enim dolorem deserunt, id a aliquam magnam esse nulla.</p>
+                    
+                    </article>
+
+                </div><!--w50-->
+
+                <div class="w50">
+                
+                    <img src="images/foto.jpg">
+
+                </div><!--w50-->
+
+            </div><!--center-->
+
+        </section><!--banner-principal-->
+
+        <section class="especialidades">
+
+            <div class="center">
+
+                <h2 class="title">Especialidades</h2>
+            
+                <article class="box-especialidade">
+                
+                    <h3>icone1</h3>
+                    <h3>CSS3</h3>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dolore ea delectus repudiandae iure ipsam magni, aperiam veritatis voluptate facere odit saepe hic necessitatibus suscipit ducimus eaque assumenda illum debitis.</p>
+                
+                </article><!--box-especialidade-->
+
+                <article class="box-especialidade">
+                
+                    <h3>icone2</h3>
+                    <h3>HTML</h3>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dolore ea delectus repudiandae iure ipsam magni, aperiam veritatis voluptate facere odit saepe hic necessitatibus suscipit ducimus eaque assumenda illum debitis.</p>
+                
+                </article><!--box-especialidade-->
+
+                <article class="box-especialidade">
+                
+                    <h3>icone3</h3>
+                    <h3>JavaScript</h3>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dolore ea delectus repudiandae iure ipsam magni, aperiam veritatis voluptate facere odit saepe hic necessitatibus suscipit ducimus eaque assumenda illum debitis.</p>
+                
+                </article><!--box-especialidade-->
+            
+            </div><!--center-->
+
+        </section><!--especialidades-->
+
+        <section class="extras">
+        
+            <div class="center">
+
+                <article class="w50">
+
+                    <h2 class="title">Depoimentos</h2>
+
+                    <article class="depoimento-single">
+                    
+                        <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus iste dicta vero illum, vel, minima cumque id reprehenderit blanditiis distinctio soluta ad, ipsa consectetur aut nobis numquam doloribus aspernatur quae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur necessitatibus harum voluptatibus repellendus temporibus saepe eum, corrupti eveniet commodi quia ex porro voluptates quos minima repellat, ipsum deleniti. Nulla?</p>
+
+                        <p class="nome-auto">Lorem Ipsum</p>
+                    
+                    </article><!--depoimento-single-->
+
+                    <article class="depoimento-single">
+                    
+                        <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus iste dicta vero illum, vel, minima cumque id reprehenderit blanditiis distinctio soluta ad, ipsa consectetur aut nobis numquam doloribus aspernatur quae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur necessitatibus harum voluptatibus repellendus temporibus saepe eum, corrupti eveniet commodi quia ex porro voluptates quos minima repellat, ipsum deleniti. Nulla?</p>
+
+                        <p class="nome-auto">Lorem Ipsum</p>
+                    
+                    </article><!--depoimento-single-->
+
+                    <article class="depoimento-single">
+                    
+                        <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus iste dicta vero illum, vel, minima cumque id reprehenderit blanditiis distinctio soluta ad, ipsa consectetur aut nobis numquam doloribus aspernatur quae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur necessitatibus harum voluptatibus repellendus temporibus saepe eum, corrupti eveniet commodi quia ex porro voluptates quos minima repellat, ipsum deleniti. Nulla?</p>
+
+                        <p class="nome-auto">Lorem Ipsum</p>
+                    
+                    </article><!--depoimento-single-->
+
+                </article><!--w50-->
+
+                <article class="w50">
+
+                    <h2 class="title">Serviços</h2>
+
+                    <div class="servicos">
+
+                        <ul>
+                        
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus iste dicta vero illum, vel, minima cumque id reprehenderit blanditiis distinctio soluta ad, ipsa consectetur aut nobis numquam doloribus aspernatur quae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur necessitatibus harum voluptatibus repellendus temporibus saepe eum, corrupti eveniet commodi quia ex porro voluptates quos minima repellat, ipsum deleniti. Nulla?</li>
+
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus iste dicta vero illum, vel, minima cumque id reprehenderit blanditiis distinctio soluta ad, ipsa consectetur aut nobis numquam doloribus aspernatur quae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur necessitatibus harum voluptatibus repellendus temporibus saepe eum, corrupti eveniet commodi quia ex porro voluptates quos minima repellat, ipsum deleniti. Nulla?</li>
+                            
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus iste dicta vero illum, vel, minima cumque id reprehenderit blanditiis distinctio soluta ad, ipsa consectetur aut nobis numquam doloribus aspernatur quae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur necessitatibus harum voluptatibus repellendus temporibus saepe eum, corrupti eveniet commodi quia ex porro voluptates quos minima repellat, ipsum deleniti. Nulla?</li>
+                        
+                        </ul>
+
+                    </div><!--servicos-->
+
+                </article><!--w50-->
+
+            </div><!--center-->
+        
+        </section><!--extras-->
+
+    </main>
+
+    <footer>
+
+        <div class="center">
+
+            <p>Todos os Direitos Reservados &copy;</p>
+
+        </div>
+    
+    </footer>
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</body>
 
 </html>
