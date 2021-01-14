fcb0f9c578e4ddd2c000649a0b5553cd27f39a55
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-01-14 18:51:33 -0300

    Fix Resize Bugs

--- App/Admin/Scripts/main.js
+++ App/Admin/Scripts/main.js
@@ -27,13 +27,37 @@ $(function () {
                     'padding' : '10px'
                 }, () => {
                     openMenu = true
+
+                    location.reload()
                 })
 
                 $('.main-container, .header-container').animate({
                     'left' : `${asideMenuSize}px`
                 }, () => {
                     openMenu = true
+
+                    location.reload()
                 })
+            }
+        })
+
+        $(window).resize(() => {
+            if (windowSize <= 768) {
+                $('.menu').css('width', '0').css('padding', '0')
+
+                $('.main-container, .header-container').css('width', '100%').css(
+                    'left', '0'
+                )
+
+                openMenu = false
+            } else {
+                openMenu = true
+
+                $('main-container').css('width', 'calc(100% - 250px)').css(
+                    'left', '250px'
+                )
+
+                $('aside.menu').css('width', '250px').css('padding', '10px', '0')
             }
         })
     }
